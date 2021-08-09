33ca11b3d2a3fcb075b018da2170d84c8d13293f
bean1310 <22887835+bean1310@users.noreply.github.com>
2021-08-10 01:04:07 +0900

    [change] remove window

--- binder/App.xaml.cs
+++ binder/App.xaml.cs
@@ -6,13 +6,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Navigation;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace binder
 {
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private TaskbarIcon taskTrayIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            taskTrayIcon = (TaskbarIcon)FindResource("TaskTrayIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+
+            taskTrayIcon.Dispose();
+            base.OnExit(e);
+        }
     }
 }
